ca74339afa82648137d8524e2565cf6c264588f3
ErkrodC <erkrodc@users.noreply.github.com>
2017-05-23 20:50:34 -0700

    Added basis of rules methods' bodies.
    Also added methods to Square class to be able to check if a square is valid, and if its occupied by a piece.

--- Assets/Scripts/AI/Rules.cs
+++ Assets/Scripts/AI/Rules.cs
@@ -4,23 +4,42 @@ namespace UnityChess.AI
 {
     public class Rules
     {
+        public static bool IsMoveLegal(Board board, Movement move, Side turn)
+        {
+            if (!move.End.IsValid()) { return false; }
+            if (!move.Piece.ValidMoves.Contains(move.End)) { return false; }
+            if (!DoesMoveRemoveCheck(board, move, turn)) { return false; }
+            if (DoesMoveCauseCheck(board, move, turn)) { return false; }
+
+            return true;
+        }
+
+        public static bool IsPlayerCheckmated(Board board, Side turn)
+        {
+            if (!IsPlayerInCheck(board, turn)) { return false; }
+
+            return true;
+        }
+
         public static bool IsPlayerInCheck(Board board, Side turn)
         {
             return false;
         }
 
-        public static bool IsPlayerCheckmated(Board board, Side turn)
+        public static bool DoesMoveRemoveCheck(Board board, Movement move, Side turn)
         {
-            return false;
+            if (!IsPlayerInCheck(board, turn)) { return true; }
+
+            Board TestBoard = new Board(board);
+            TestBoard.MovePiece(move);
+            TestBoard.UpdateAllPiecesValidMoves();
+
+            return !IsPlayerInCheck(TestBoard, turn);
         }
 
-        public static bool DoesMoveRemoveCheck(Board board, Movement move)
+        public static bool DoesMoveCauseCheck(Board board, Movement move, Side turn)
         {
-            return false;
-        }
 
-        public static bool IsMoveLegal(Board board, Movement move, Side turn)
-        {
             return false;
         }
     }
