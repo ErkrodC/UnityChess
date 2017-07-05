6eba081ed2fb232cc7929b2a1fcda75f5227cf88
ErkrodC <erkrodc@users.noreply.github.com>
2017-07-05 09:20:18 -0700

    Fixed order of BasePieceList following SetStartingPosition call
    also redid main board comment for simplicity

--- Assets/Scripts/Base/Board.cs
+++ Assets/Scripts/Base/Board.cs
@@ -17,17 +17,17 @@ namespace UnityChess
         Boards will be represented as a 10x12 grid in a one dimensional list.
         The 8x8 chessboard is in the center, viewed from above with white on the side closer to index 0.
 
+             110  111 112 ... 117 118  119                                  Compass:             (0, 1)
+             100  101 102 ... 107 108  109                                                        Black
+                |---------------------|               ^                    (-1, 1) Black-Queenside  |   Black-Kingside (1, 1)
+               .|              .   98 | 99          R |                                          \  |  /
+               .|            .        | .           a |               (-1,0) Queenside--------------*------------------Kingside (1, 0)
+               .|          .          | .           n |                                          /  |  \
+              30| 31 32 33 ...        | 39          k |                  (-1, -1) White-Queenside   |   White-Kingside (1, -1)
+              20| 21 22 23 ...        | 29            ------------>                               White
+                |---------------------|         (1,1)       file                                 (0, -1)
+              10  11 12 13  ... 17 18   19
                0   1  2  3  ...  7  8   9
-              10  11 12 13  ... 17 18   19                                  Compass:           (0, -1)
-                |---------------------|               file    (1,1)                             White
-              20| 21 22 23 ...        | 29      <------------             (1,-1) White-Kingside   |     White-Queenside (-1,-1)
-              30| 31 32 33 ...        | 39                  | R                                \  |  /
-               .|          .          | .                   | a       (1,0) Kingside--------------*------------------Queenside (-1, 0)
-               .|            .        | .                   | n                                /  |  \
-               .|              .   98 | 99                  | k            (1,1) Black-Kingside   |     Black-Queenside (-1,1)
-                |---------------------|                     V                                   Black
-             100  101 102 ... 107 108  109                                                      (0,1)
-             110  111 112 ... 117 118  119
         */
         public List<BasePiece> BasePieceList { get; set; }
 
@@ -45,13 +45,14 @@ namespace UnityChess
         /// </summary>
         public Board(Board board)
         {
+            this.BasePieceList = new List<BasePiece>(Enumerable.Range(0, 120).Select(i => (BasePiece)null));
             this.SetBlankBoard();
 
             //creates deep copy (makes copy of each piece and deep copy of their respective ValidMoves lists) of board (list of BasePiece's)
             //this may be a memory hog since each Board has a list of Piece's, and each piece has a list of Movement's
             //avg number turns/Board's per game should be around ~80. usual max number of pieces per board is 32
             // TODO optimize this method
-            foreach (Piece piece in BasePieceList.OfType<Piece>())
+            foreach (Piece piece in board.BasePieceList.OfType<Piece>())
             {
                 BasePieceList[piece.Position.AsIndex()] = piece.Clone();
             }
@@ -92,28 +93,28 @@ namespace UnityChess
             //Row 2/Rank 7 and Row 7/Rank 2, both rows of pawns
             for (int i = 31; i <= 38; i++)
             {
-                this.BasePieceList[i] = new Pawn(new Square(i), Side.Black);
-                this.BasePieceList[i + 50] = new Pawn(new Square(i + 50), Side.White);
+                this.BasePieceList[i] = new Pawn(new Square(i), Side.White);
+                this.BasePieceList[i + 50] = new Pawn(new Square(i + 50), Side.Black);
             }
 
             //Rows 1 & 8/Ranks 8 & 1, back rows for both players
-            this.BasePieceList[21] = new Rook(new Square(21), Side.Black);
-            this.BasePieceList[22] = new Knight(new Square(22), Side.Black);
-            this.BasePieceList[23] = new Bishop(new Square(23), Side.Black);
-            this.BasePieceList[24] = new Queen(new Square(24), Side.Black);
-            this.BasePieceList[25] = new King(new Square(25), Side.Black);
-            this.BasePieceList[26] = new Bishop(new Square(26), Side.Black);
-            this.BasePieceList[27] = new Knight(new Square(27), Side.Black);
-            this.BasePieceList[28] = new Rook(new Square(28), Side.Black);
+            this.BasePieceList[21] = new Rook(new Square(21), Side.White);
+            this.BasePieceList[22] = new Knight(new Square(22), Side.White);
+            this.BasePieceList[23] = new Bishop(new Square(23), Side.White);
+            this.BasePieceList[24] = new Queen(new Square(24), Side.White);
+            this.BasePieceList[25] = new King(new Square(25), Side.White);
+            this.BasePieceList[26] = new Bishop(new Square(26), Side.White);
+            this.BasePieceList[27] = new Knight(new Square(27), Side.White);
+            this.BasePieceList[28] = new Rook(new Square(28), Side.White);
 
-            this.BasePieceList[91] = new Rook(new Square(91), Side.White);
-            this.BasePieceList[92] = new Knight(new Square(92), Side.White);
-            this.BasePieceList[93] = new Bishop(new Square(93), Side.White);
-            this.BasePieceList[94] = new Queen(new Square(94), Side.White);
-            this.BasePieceList[95] = new King(new Square(95), Side.White);
-            this.BasePieceList[96] = new Bishop(new Square(96), Side.White);
-            this.BasePieceList[97] = new Knight(new Square(97), Side.White);
-            this.BasePieceList[98] = new Rook(new Square(98), Side.White);
+            this.BasePieceList[91] = new Rook(new Square(91), Side.Black);
+            this.BasePieceList[92] = new Knight(new Square(92), Side.Black);
+            this.BasePieceList[93] = new Bishop(new Square(93), Side.Black);
+            this.BasePieceList[94] = new Queen(new Square(94), Side.Black);
+            this.BasePieceList[95] = new King(new Square(95), Side.Black);
+            this.BasePieceList[96] = new Bishop(new Square(96), Side.Black);
+            this.BasePieceList[97] = new Knight(new Square(97), Side.Black);
+            this.BasePieceList[98] = new Rook(new Square(98), Side.Black);
         }
 
         /// <summary>
