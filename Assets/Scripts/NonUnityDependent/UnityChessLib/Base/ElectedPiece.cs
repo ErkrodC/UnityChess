407768b4f4434c2e02bfbdc2ae9cf10b8080403a
Eric R <erkrodc@gmail.com>
2019-04-20 13:41:11 -0700

    Remove UnityChessLib submod code.

--- Assets/Scripts/NonUnityDependent/UnityChessLib/Base/ElectedPiece.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UnityChess {
-	public enum ElectedPiece {
-		Knight = 0,
-		Bishop = 1,
-		Rook = 2,
-		Queen = 3,
-		None = -1
-	}
-}
--- Assets/Scripts/NonUnityDependent/UnityChessLib/Base/Mode.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UnityChess {
-	/// <summary>Describes whether each player is either a Human or AI.</summary>
-	public enum Mode {
-		HumanVsHuman = 0,
-		HumanVsAI = 1,
-		AIvsAI = 2
-	}
-}
