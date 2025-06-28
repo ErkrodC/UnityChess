fef8edff3243dee6bdacf01534f74eb1626b8e72
erodri <erkrodc@users.noreply.github.com>
2025-06-28 02:00:57 -0700

    Updated drag handlers to new input system

--- Assets/Scripts/Game/VisualPiece.cs
+++ Assets/Scripts/Game/VisualPiece.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections.Generic;
 using UnityChess;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using static UnityChess.SquareUtil;
 
-public class VisualPiece : MonoBehaviour {
+public class VisualPiece : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler {
 	public delegate void VisualPieceMovedAction(Square movedPieceInitialSquare, Transform movedPieceTransform, Transform closestBoardSquareTransform, Piece promotionPiece = null);
 	public static event VisualPieceMovedAction VisualPieceMoved;
-	
+
 	public Side PieceColor;
 	public Square CurrentSquare => StringToSquare(transform.parent.name);
-	
+
 	private const float SquareCollisionRadius = 9f;
 	private Camera boardCamera;
 	private Vector3 piecePositionSS;
@@ -23,43 +24,43 @@ public class VisualPiece : MonoBehaviour {
 		boardCamera = Camera.main;
 	}
 
-	public void OnMouseDown() {
-		if (enabled) {
-			piecePositionSS = boardCamera.WorldToScreenPoint(transform.position);
-		}
+	public void OnPointerDown(PointerEventData eventData) {
+		if (!enabled) { return; }
+
+		piecePositionSS = boardCamera.WorldToScreenPoint(transform.position);
 	}
 
-	private void OnMouseDrag() {
-		if (enabled) {
-			Vector3 nextPiecePositionSS = new Vector3(Input.mousePosition.x, Input.mousePosition.y, piecePositionSS.z);
-			thisTransform.position = boardCamera.ScreenToWorldPoint(nextPiecePositionSS);
-		}
+	public void OnDrag(PointerEventData eventData) {
+		if (!enabled) { return; }
+
+		Vector3 nextPiecePositionSS = new Vector3(eventData.position.x, eventData.position.y, piecePositionSS.z);
+		thisTransform.position = boardCamera.ScreenToWorldPoint(nextPiecePositionSS);
 	}
 
-	public void OnMouseUp() {
-		if (enabled) {
-			potentialLandingSquares.Clear();
-			BoardManager.Instance.GetSquareGOsWithinRadius(potentialLandingSquares, thisTransform.position, SquareCollisionRadius);
+	public void OnPointerUp(PointerEventData eventData) {
+		if (!enabled) { return; }
 
-			if (potentialLandingSquares.Count == 0) { // piece moved off board
-				thisTransform.position = thisTransform.parent.position;
-				return;
-			}
-	
-			// determine closest square out of potential landing squares.
-			Transform closestSquareTransform = potentialLandingSquares[0].transform;
-			float shortestDistanceFromPieceSquared = (closestSquareTransform.transform.position - thisTransform.position).sqrMagnitude;
-			for (int i = 1; i < potentialLandingSquares.Count; i++) {
-				GameObject potentialLandingSquare = potentialLandingSquares[i];
-				float distanceFromPieceSquared = (potentialLandingSquare.transform.position - thisTransform.position).sqrMagnitude;
+		potentialLandingSquares.Clear();
+		BoardManager.Instance.GetSquareGOsWithinRadius(potentialLandingSquares, thisTransform.position, SquareCollisionRadius);
 
-				if (distanceFromPieceSquared < shortestDistanceFromPieceSquared) {
-					shortestDistanceFromPieceSquared = distanceFromPieceSquared;
-					closestSquareTransform = potentialLandingSquare.transform;
-				}
-			}
-
-			VisualPieceMoved?.Invoke(CurrentSquare, thisTransform, closestSquareTransform);
+		if (potentialLandingSquares.Count == 0) { // piece moved off board
+			thisTransform.position = thisTransform.parent.position;
+			return;
 		}
+
+		// determine closest square out of potential landing squares.
+		Transform closestSquareTransform = potentialLandingSquares[0].transform;
+		float shortestDistanceFromPieceSquared = (closestSquareTransform.transform.position - thisTransform.position).sqrMagnitude;
+		for (int i = 1; i < potentialLandingSquares.Count; i++) {
+			GameObject potentialLandingSquare = potentialLandingSquares[i];
+			float distanceFromPieceSquared = (potentialLandingSquare.transform.position - thisTransform.position).sqrMagnitude;
+
+			if (distanceFromPieceSquared < shortestDistanceFromPieceSquared) {
+				shortestDistanceFromPieceSquared = distanceFromPieceSquared;
+				closestSquareTransform = potentialLandingSquare.transform;
+			}
+		}
+
+		VisualPieceMoved?.Invoke(CurrentSquare, thisTransform, closestSquareTransform);
 	}
 }
