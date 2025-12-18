60504f1800eb96f6df5b25bf70ced79af71273b0
erodri <erkrodc@users.noreply.github.com>
2025-12-18 02:25:58 -0800

    Update namespace import in `PromotionInteraction` to `UnityChess.Core.Util`

--- Assets/Scripts/Application/PromotionInteraction.cs
+++ Assets/Scripts/Application/PromotionInteraction.cs
@@ -2,7 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityChess.Core;
-using UnityChess.Util;
+using UnityChess.Core.Util;
 
 namespace UnityChess.Application {
 	public sealed class PromotionInteraction : IDisposable {
